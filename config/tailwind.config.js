f372ab191e3a042bb15bab7c69c8af82d6419437
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-04 22:42:38 +0200

    style articles

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,29 +1,30 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  content: [
-    './public/*.html',
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
-  ],
-  theme: {
-    colors: {
-      'gb-dark': '#1A202C',
-      'primary': '#F6AE2D',
-      'secondary': '#3B4A67',
-      'text-light': '#FFFFFF',
-      'text-dark': '#000000',
+    content: [
+        './public/*.html',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/views/**/*.{erb,haml,html,slim}'
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'gb-dark': '#1A202C',
+                'primary': '#F6AE2D',
+                'secondary': '#3B4A67',
+                'light': '#FFFFFF',
+                'dark': '#000000',
+            },
+            fontFamily: {
+                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+            }
+
+        },
     },
-    extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/aspect-ratio'),
+        require('@tailwindcss/typography'),
+    ]
 }
