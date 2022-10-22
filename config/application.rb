2dccd1fda847fdb68a7a9e2689031e8abd317315
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-22 19:58:02 +0200

    add basic hu localization

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module KbprWeb
   class Application < Rails::Application
+    config.i18n.default_locale = :hu
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
