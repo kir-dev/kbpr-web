f277fc780c852004c0739a3faa170b0c73b92aed
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-25 20:21:01 +0200

    add default false to user admin

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_04_195019) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_23_210035) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -60,7 +60,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_04_195019) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.string "email"
     t.string "authsch_id"
     t.datetime "created_at", null: false
