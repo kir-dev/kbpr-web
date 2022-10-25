596d9dd84093221cf69efd4727f9fec9ac89a39a
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-25 20:21:17 +0200

    implement user pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :articles
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
-  resources :users
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
