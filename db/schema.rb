5573571ddf7d97c3e11cbb9b316d685969f1ee2f
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-10-29 19:23:12 +0200

    add design order page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_201103) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_164718) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_201103) do
     t.boolean "printed_by_me"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "group_id", null: false
+    t.index ["group_id"], name: "index_orders_on_group_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
@@ -94,5 +96,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_25_201103) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "orders", "groups"
   add_foreign_key "orders", "users"
 end
