bf799fabc32589e62c28f6692c79d59215d46b72
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2022-11-24 15:31:04 +0100

    remove host

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module KbprWeb
     config.i18n.default_locale = :hu
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.hosts << "vstr.sch.bme.hu"
 
     # Configuration for the application, engines, and railties goes here.
     #
