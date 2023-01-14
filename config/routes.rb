57a002266cc8cafafbe0a5f8bcd225c7d1e96f42
SepsiLaszlo <sepsi.laszlo99@gmail.com>
2023-01-14 12:02:36 +0100

    update order form

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :order_items
   resources :items
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   resources :groups
   resources :orders do
-    get :new_item, on: :member
-    post :create_item, on: :member
-    post :update_item, on: :member
-    get :delete_item, on: :member
-    get :all_item, on: :member
-
+    patch :finalize, on: :member
   end
+
   resources :articles
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
