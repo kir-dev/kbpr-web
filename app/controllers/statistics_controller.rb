c921de3a3140cea0636e9c868bdd07d6c98f48e9
Andai Roland <r.andai@outlook.com>
2023-03-24 14:10:42 +0100

    user statistics' pages created

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -35,9 +35,34 @@ class StatisticsController < ApplicationController
     end
   end
 
+  def for_user_index
+    user_stat()
+  end
+
+  def for_user
+    user_stat()
+    @user = User.find(params[:user_id])
+    @stat = @users[@user]
+  end
+
   private
 
   def fiscal_period_id
     params[:fiscal_period_id]
+  end
+
+  def user_stat
+    @fiscal_period = fiscal_period_id ? FiscalPeriod.find(fiscal_period_id) : FiscalPeriod.last
+    @order_items = OrderItem.joins(:order).includes(:item, order: :completed_by)
+                            .where('orders.finalized_at BETWEEN :start and :end',
+                                   start: @fiscal_period.start_at, end: @fiscal_period.end_at)
+    @users = {}
+    @order_items.each do |order_item|
+      user = order_item.order.completed_by
+      item = order_item.item
+      @users[user] ||= {}
+      @users[user][item] ||= 0
+      @users[user][item] = @users[user][item] + order_item.quantity
+    end
   end
 end
