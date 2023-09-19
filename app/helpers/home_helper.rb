dfa13c0c17b7d699e2b91978e7dfddd6fd9f3617
Bende126 <bendi2001@gmail.com>
2023-09-19 21:37:14 +0000

    Funky text instead of lorem ipsum

---
+++ app/helpers/home_helper.rb
@@ -0,0 +1,16 @@
+module HomeHelper
+    def randomText
+     # pick one of them randomly
+     ["Ahol a körvez csinál mindent🧢", 
+        "A dizájnt kérd az Schdesign-tól", 
+        "Nyomtatunk és elvagyunk", 
+        "Olvasd a híreinket!👇", 
+        "Figyelj a nyomtatási keretedre!",
+        "Random szöveg generátor\nmodel: KB-PR🌐",
+        "Köszönet a Kir-Dev-nek a weboldalért!😘",
+        "Nem értek a Rubyhoz",
+        "Új logó mikor???"
+    ].sample
+    end
+  end
+  
