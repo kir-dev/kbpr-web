4239fd2e3fc334640c8825d5fc0b05c0d224bf7c
bbucsy <bucsy.beni@gmail.com>
2024-01-30 18:32:24 +0100

    change notification server

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -1,7 +1,7 @@
 module OrdersHelper
     def send_notification(order)
         if Rails.env.production?
-            uri = URI('https://ntfy.noeyouth.eu/kbpr')
+            uri = URI('https://ntfy.sh/kbpr')
             body = "#{Group.find_by(id: order["group_id"]).name} rendelt plakátot. #{if order["printed_by_me"] == "1" then "Ők nyomtatják" else "Kbpr nyomtatja" end}"
             headers = { 'Authorization' => 'Bearer ' + Rails.application.credentials.notification_api_key,
                     'Title' => "Új rendelés",
