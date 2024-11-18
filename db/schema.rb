71fd244ce80b76f5660b7f8fca1f44b41a1602dc
Bende126 <bendi2001@gmail.com>
2024-11-18 08:57:03 +0000

    add item weight to schema and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_01_152041) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_15_223838) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_01_152041) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "available", default: true
+    t.integer "munkapont_weight", default: 1
   end
 
   create_table "order_items", force: :cascade do |t|
