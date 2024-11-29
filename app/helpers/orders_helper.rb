83168e2eb5f2939a78278301cc6ed1e3ed6d7b12
zsotroav <zsotroav@gmail.com>
2024-11-29 12:29:43 +0100

    Update notification strings
    Én nyomtatom bepipálása esetén könnyebben megkülönböztethető értesítések:
    Végén Kbpr/Kör nyomtatja -> redelt/nyomtat kulcsszó használata
    Új rendelés cím -> Egy kör nyomtat / Új rendelés
    Emoji: 🖨 ha én nyomtatom, 📥 ha rendelem
    Tag: `uj-rendeles` és `uj-nyomtatas` tagekre szétszedve

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -2,10 +2,10 @@ module OrdersHelper
     def send_notification(order)
         if Rails.env.production?
             uri = URI('https://ntfy.noeyouth.eu/kbpr')
-            body = "#{Group.find_by(id: order["group_id"]).name} rendelt plakátot. #{if order["printed_by_me"] == "1" then "Ők nyomtatják" else "Kbpr nyomtatja" end}"
+            body = "#{Group.find_by(id: order["group_id"]).name} #{if order["printed_by_me"] == "1" then "nyomtat" else "rendelt" end} plakátot."
             headers = { 'Authorization' => 'Bearer ' + Rails.application.credentials.notification_api_key,
-                    'Title' => "Új rendelés",
-                    'Tags' => 'inbox_tray, uj-rendeles',
+                    'Title' => "#{if order["printed_by_me"] == "1" then "Egy kör nyomtat" else "Új rendelés" end}",
+                    'Tags' => '#{if order["printed_by_me"] == "1" then "printer, uj-nyomtatas" else "inbox_tray, uj-rendeles" end}',
                     'Priority' =>  'default',
                     'Action' => 'view, Megnyitás, https://kbpr.sch.bme.hu/orders, clear=true'}
             response = Net::HTTP.post(uri, body.to_json, headers)
