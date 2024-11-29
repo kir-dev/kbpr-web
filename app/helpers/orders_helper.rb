7d54d200ab54e23e918a79df8cceb3c440b44515
zsotroav <zsotroav@gmail.com>
2024-11-29 15:59:13 +0100

    fix ' to "

--- app/helpers/orders_helper.rb
+++ app/helpers/orders_helper.rb
@@ -5,7 +5,7 @@ module OrdersHelper
             body = "#{Group.find_by(id: order["group_id"]).name} #{if order["printed_by_me"] == "1" then "nyomtat" else "rendelt" end} plakátot."
             headers = { 'Authorization' => 'Bearer ' + Rails.application.credentials.notification_api_key,
                     'Title' => "#{if order["printed_by_me"] == "1" then "Egy kör nyomtat" else "Új rendelés" end}",
-                    'Tags' => '#{if order["printed_by_me"] == "1" then "printer, uj-nyomtatas" else "inbox_tray, uj-rendeles" end}',
+                    'Tags' => "#{if order["printed_by_me"] == "1" then "printer, uj-nyomtatas" else "inbox_tray, uj-rendeles" end}",
                     'Priority' =>  'default',
                     'Action' => 'view, Megnyitás, https://kbpr.sch.bme.hu/orders, clear=true'}
             response = Net::HTTP.post(uri, body.to_json, headers)
